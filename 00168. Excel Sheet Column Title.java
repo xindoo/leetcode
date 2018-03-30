8c397dea1e01dbc9fcdf007bf06ad41c73f7f19c
xindoo <xindoo@qq.com>
2018-03-30 15:31:15 +0800

    add 168

--- 00168. Excel Sheet Column Title.java
+++ 00168. Excel Sheet Column Title.java
@@ -1,9 +1,12 @@
 class Solution {
     public String convertToTitle(int n) {
+        char[] arr={'Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y'};
         StringBuffer sb = new StringBuffer();
         while (n > 0) {
-            sb.append((char)('A'+n%26-1));
-            n /= 26;
+            int i=n%26;
+            sb.append(arr[i]);
+            if(i==0) n = n-1;
+            n/=26;
         }
         return new String(sb.reverse());
     }
