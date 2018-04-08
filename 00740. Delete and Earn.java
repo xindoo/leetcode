57e4d881a0cfcdade438ad05a14fc183ffdae40f
xindoo <xindoo@qq.com>
2018-04-08 17:21:22 +0800

    add 740

--- 00740. Delete and Earn.java
+++ 00740. Delete and Earn.java
@@ -1,17 +1,17 @@
-import java.util.Arrays;
-
 class Solution {
     public int deleteAndEarn(int[] nums) {
         int n = 10000;
+        int maxval = 0;
         int[] arr = new int[n+1];
         for (int i = 0; i < nums.length; i++) {
             arr[nums[i]] += nums[i];
+            maxval = Math.max(maxval, nums[i]);
         }
-        int[][] dp = new int[n+1][2];
-        for (int i = 1; i <= n; i++) {
+        int[][] dp = new int[maxval+1][2];
+        for (int i = 1; i <= maxval; i++) {
             dp[i][1] = dp[i-1][0] + arr[i];
             dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1]);
         }
-        return Math.max(dp[n][0], dp[n][1]);
+        return Math.max(dp[maxval][0], dp[maxval][1]);
     }
 }
