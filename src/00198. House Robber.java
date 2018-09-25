c8f68cae5813f26edd2e55cfba615a0e5076fa3c
xindoo <xindoo@qq.com>
2018-09-25 19:04:30 +0800

    update 198

--- src/00198. House Robber.java
+++ src/00198. House Robber.java
@@ -1,22 +1,13 @@
-public class Solution {
+class Solution {
     public int rob(int[] nums) {
-        if (0 == nums.length)
+        if (nums == null || nums.length == 0) {
             return 0;
-        int[][] dp = new int[2][nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            if (0 == i) {
-                dp[0][0] = 0;
-                dp[1][0] = nums[0];
-            }
-            else if (1 == i) {
-                dp[0][1] = nums[0];
-                dp[1][1] = nums[1];
-            }
-            else {
-                dp[0][i] = Math.max(dp[0][i-1], dp[1][i-1]);
-                dp[1][i] = Math.max(dp[1][i-1], dp[0][i-1]+nums[i]);
-            }
         }
-        return Math.max(dp[0][nums.length-1], dp[1][nums.length-1]); 
+        int[][] dp = new int[nums.length+1][2];
+        for(int i = 0; i < nums.length; i++) {
+            dp[i+1][1] = Math.max(dp[i][1], dp[i][0] + nums[i]);
+            dp[i+1][0] = Math.max(dp[i][0], dp[i][1]);
+        }
+        return Math.max(dp[nums.length][0], dp[nums.length][1]);
     }
 }
